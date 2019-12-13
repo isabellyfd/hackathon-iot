2f10a7ffa229e36a3052a49f34c6ed677d30ad7a
isadamascena <idamasce@thoughtworks.com>
2019-12-13 18:39:25 -0300

    Add call to server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DashboardNavbar from './components/DashboardNavbar';
 import Tab from './components/Tab'
+
 function App() {
   return (
     <div className="App">
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+// import Button from 'react-bootstrap/Button'
 
 class Tab extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            temperature: "22C"
+            temperature: 25,
         }
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(()=> this.changeTemperature(), 1000);
+    }
+
+    componentWillMount() {
+        clearInterval(this.timer)
+        this.timer = null;
+    }
+
+    changeTemperature = () => {
+        fetch("http://localhost:8585")
+            .then(result => result.json())
+            .then(result =>  this.setState({temperature: result.temperature}))
     }
 
     render() {
         return (
-            <div className="tab">Temperature Sensor {this.state.temperature}</div>
+            <div className="tab">
+                Temperature Sensor {this.state.temperature}C
+                {/* <Button variant="dark" onClick={this.handleRefreshButton}>Refresh</Button> */}
+            </div>
+        
         );
     }
 }
